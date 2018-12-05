252ebbc90dc686d93506df561ab1262400c3886b
Angelita Sasmita <angelita.sasmita@gmail.com>
2018-12-05 20:21:38 +0800

    [Angie] figure out how to update drawer with state

--- lib/song_selector_screen.dart
+++ lib/song_selector_screen.dart
@@ -6,17 +6,62 @@ class SongSelectorScreen extends StatefulWidget {
 }
 
 class SongSelectorScreenState extends State<SongSelectorScreen> {
+  Widget _showScreen;
+
+  @override
+  void initState(){
+    super.initState();
+    _showScreen = _buildCategoryList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        Text('hi'),
-        Text('hi'),
-        Text('hi'),
-        Text('hi'),
-        Text('hi'),
-        Text('hi'),
-      ],
+    String _currentScreen = "category";
+
+    return _showScreen;
+  }
+
+
+
+
+  Widget _buildSingerList(String title) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView(children: <Widget>[
+          ListTile(
+            leading: Text(
+              'Female Singers',
+//              style: Theme.of(context).textTheme.subhead,
+            ),
+          ),
+        ]));
+  }
+
+  Widget _buildCategoryList(){
+     return Scaffold(
+      appBar: AppBar(
+        title: Text("Category"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Text(
+              'Female Singers',
+//              style: Theme.of(context).textTheme.subhead,
+            ),
+            onTap: () {
+              setState(() {
+                _showScreen = _buildSingerList("h");
+              });
+            },
+          ),
+          ListTile(
+            leading: Text('Male Singers'),
+          )
+        ],
+      ),
     );
   }
 }
