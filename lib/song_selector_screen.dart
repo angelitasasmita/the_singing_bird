4e32227a2c5aecd7cd61e946d854191a56311a27
Angelita Sasmita <angelita.sasmita@gmail.com>
2018-12-06 14:35:29 +0800

    [Angie] done with populating buildcategory, buildsingerlist and buildsinglist with json data

--- lib/song_selector_screen.dart
+++ lib/song_selector_screen.dart
@@ -5,6 +5,8 @@ import 'package:the_singing_bird/category.dart';
 import 'package:the_singing_bird/singer.dart';
 import 'package:the_singing_bird/song.dart';
 
+final _categories = <Category>[];
+
 class SongSelectorScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => SongSelectorScreenState();
@@ -12,10 +14,9 @@ class SongSelectorScreen extends StatefulWidget {
 
 class SongSelectorScreenState extends State<SongSelectorScreen> {
   Widget _showScreen;
-  final _categories = <Category>[];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _showScreen = _buildCategoryList();
   }
@@ -30,7 +31,6 @@ class SongSelectorScreenState extends State<SongSelectorScreen> {
   @override
   Future<void> didChangeDependencies() async {
     super.didChangeDependencies();
-    print(_categories.length);
     if (_categories.isEmpty) {
       await _retrieveLocalCategories();
     }
@@ -38,64 +38,74 @@ class SongSelectorScreenState extends State<SongSelectorScreen> {
 
   /// Retrieves a list of [Categories] and their [Unit]s
   Future<void> _retrieveLocalCategories() async {
-    final json = DefaultAssetBundle
-        .of(context)
-        .loadString('assets/data/songs.json');
+    final json =
+        DefaultAssetBundle.of(context).loadString('assets/data/songs.json');
     final data = JsonDecoder().convert(await json);
     if (data is! Map) {
       throw ('Data retrieved from API is not a Map');
     }
     data.keys.forEach((key) {
-
-      final List<Singer> singers = data[key].map<Singer>((i) => Singer.fromJSON(i)).toList();
-      var category = Category(
-        name: key,
-        singers: singers
-      );
+      final List<Singer> singers =
+          data[key].map<Singer>((i) => Singer.fromJSON(i)).toList();
+      var category = Category(name: key, singers: singers);
 
       _categories.add(category);
     });
   }
 
-
-  Widget _buildSingerList(String title) {
+  Widget _buildSongList(String title, int categoryIndex, int singerIndex) {
     return Scaffold(
         appBar: AppBar(
           title: Text(title),
         ),
-        body: ListView(children: <Widget>[
-          ListTile(
-            leading: Text(
-              'Female Singers',
-//              style: Theme.of(context).textTheme.subhead,
-            ),
-          ),
-        ]));
+        body: ListView.builder(
+            itemCount: _categories[categoryIndex].singers[singerIndex].songs.length,
+            itemBuilder: (context, int) {
+              return ListTile(
+                leading: Text(_categories[categoryIndex].singers[singerIndex].songs[int].title),
+              );
+            }));
   }
 
-  Widget _buildCategoryList(){
-     return Scaffold(
-      appBar: AppBar(
-        title: Text("Category"),
-      ),
-      body: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Text(
-              'Female Singers',
-//              style: Theme.of(context).textTheme.subhead,
-            ),
-            onTap: () {
-              setState(() {
-                _showScreen = _buildSingerList("h");
-              });
-            },
-          ),
-          ListTile(
-            leading: Text('Male Singers'),
-          )
-        ],
-      ),
-    );
+  Widget _buildSingerList(String title, int categoryIndex) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView.builder(
+            itemCount: _categories[categoryIndex].singers.length,
+            itemBuilder: (context, int) {
+              var singerName = _categories[categoryIndex].singers[int].name;
+
+              return ListTile(
+                leading: Text(singerName),
+                onTap: () {
+                  setState(() {
+                    _showScreen = _buildSongList(singerName, categoryIndex, int);
+                  });
+                },
+              );
+            }));
+  }
+
+  Widget _buildCategoryList() {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Category"),
+        ),
+        body: ListView.builder(
+            itemCount: _categories.length,
+            itemBuilder: (context, int) {
+              var categoryName = _categories[int].name;
+
+              return ListTile(
+                leading: Text(categoryName),
+                onTap: () {
+                  setState(() {
+                    _showScreen = _buildSingerList(categoryName, int);
+                  });
+                },
+              );
+            }));
   }
 }
