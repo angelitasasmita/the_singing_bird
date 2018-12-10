6475e769b230e955f6ecd154c9cf28d34be079b2
Angelita Sasmita <angelita.sasmita@gmail.com>
2018-12-10 14:50:55 +0800

    [Angie] add FAB, but still not working

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -16,33 +16,33 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     final MyInheritedWidgetState state = MyInheritedWidget.of(context);
     Widget _videoScreen;
 
-
     if (state.songsCount == 0) {
-      _videoScreen = Center(child: Text(':('));
-
+      _videoScreen = Center(child: Text('No songs playing :('));
     } else {
       _videoScreen = WebviewScaffold(url: state.retriveSongs[0].link);
     }
 
-    return
-      Expanded(
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("The Singing Bird"),
-            automaticallyImplyLeading: false,
-          ),
-          body: Container(
-            height: 300,
-            child: _videoScreen
-
+    return Expanded(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("The Singing Bird"),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+        ),
+        body: Container(height: 300, child: _videoScreen),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.skip_next),
+            onPressed: () {
+              setState(() {
+                state.removeSong(state.retriveSongs[0]);
+              });
+            }),
 
 //      Container(
 //      height: 300,
 //        child: FlutterYoutube.playYoutubeVideoByUrl(
 //            apiKey: _API_KEY, videoUrl: state.retriveSongs[0].link, autoPlay: true)
-
-              ),
-    ),
-      );
+      ),
+    );
   }
 }
