4bbb324070ba8ee81f74132ca1b59022cbd99e11
Angelita Sasmita <angelita.sasmita@gmail.com>
2018-12-10 13:59:19 +0800

    [Angie] try out different ways for embeded youtube video

--- lib/video_player_screen.dart
+++ lib/video_player_screen.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube/flutter_youtube.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+final _API_KEY = "AIzaSyDmissqr2kSD0ywG6r9GCr0DxCDqIOzq90";
 
 class VideoPlayerScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _VideoPlayerScreenState();
-
-
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        shrinkWrap: true,
-        children: <Widget>[
-          Text('hi'),
-          Text('hi'),
-          Text('hi'),
-          Text('hi'),
-          Text('hi'),
-          Text('hi'),
-        ],
-      ),
+    return Container(
+      height: 300,
+      child: WebviewScaffold(url: "https://www.youtube.com/watch?v=HblwLcAR4r4"
+
+
+//      Container(
+//      height: 300,
+//        child: FlutterYoutube.playYoutubeVideoByUrl(
+//            apiKey: _API_KEY, videoUrl: "https://www.youtube.com/watch?v=HblwLcAR4r4", autoPlay: true)
+
+          ),
     );
   }
-
 }
